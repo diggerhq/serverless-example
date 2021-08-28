de6b1f650b72a6db6da41e24aaa3c048d6b76b40
motatoes <moe.habib9@gmail.com>
2021-08-28 17:18:45 +0300

    include handler fix syntax

--- index.js
+++ index.js
@@ -2,10 +2,32 @@
 console.log('Loading hello world function');
 
 
-export function handler(event) {
+exports.handler = async function(event) {
     let responseCode = 200;
     let responseBody = {
         message: "hello from digger!",
+        input: event
+    };
+    
+    // The output from a Lambda proxy integration must be 
+    // in the following JSON object. The 'headers' property 
+    // is for custom response headers in addition to standard 
+    // ones. The 'body' property  must be a JSON string. For 
+    // base64-encoded payload, you must also set the 'isBase64Encoded'
+    // property to 'true'.
+    let response = {
+        statusCode: responseCode,
+        headers: {},
+        body: JSON.stringify(responseBody)
+    };
+    console.log("response: " + JSON.stringify(response))
+    return response;
+}
+
+exports.handle2 = async function(event) {
+    let responseCode = 200;
+    let responseBody = {
+        message: "hello from digger2!",
         input: event
     };
     
